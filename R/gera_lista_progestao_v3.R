2960f3988a7556ef70c289cf7f1cf75c57afd4ce
marcosfs2006 <marcosfs2006@gmail.com>
2023-02-14 17:08:13 +0000

    Atualização do painel em razão da atualização da base de dados.

--- R/gera_lista_progestao_v3.R
+++ R/gera_lista_progestao_v3.R
@@ -6,10 +6,10 @@ library(dplyr)
 library(lubridate)
 
 
-dt_lista <- "2023-01-26"
+dt_lista <- "2023-02-10"
 
 # extracao dos dados
-progestao <- extract_tables("pdf-lista-progestao/PROGESTAORELAOENTES26012023.pdf")
+progestao <- extract_tables("pdf-lista-progestao/PROGESTAORELAOENTES10022023.pdf")
 progestao <- as.data.frame(do.call(rbind, progestao))
 progestao <- progestao[-(1:2),-1] # exclui 1a linha e 1a coluna
 names(progestao) <- c("cnpj",
